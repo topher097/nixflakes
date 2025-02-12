1e70483616e50f9d8b0ceb9b7f0d94e86b07615c
topher097 <cmendres400@gmail.com>
2025-02-12 14:22:10 -0500

    Add tailscale magic DNS stuff

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -1,4 +1,5 @@
-{ username, ... }:
+# LINK: https://tailscale.com/download/linux/nixos
+{ username, hostName, pkgs, ... }:
 {
   services.tailscale = {
     enable = true;
@@ -7,4 +8,9 @@
       "--operator=${username}"
     ];
   };
+
+  # Setup the magic DNS
+  networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
+  networking.search = [ "tail8dc3e.ts.net" ];   # Found in the DNS section on tailscale admin console
+
 }
