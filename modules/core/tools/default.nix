67764851833019702bbce101b30e8ebf0423ea3e
topher097 <cmendres400@gmail.com>
2025-02-23 21:19:07 -0500

    Add devenv extra config for trusted user access

--- modules/core/tools/default.nix
+++ modules/core/tools/default.nix
@@ -40,7 +40,12 @@
     ntfs3g
     exfat
   ];
-
+  
+  nix.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=
+  '';
+  
   # Git aliases
   home-manager.users.${username}.programs.git = {
     enable = true;
