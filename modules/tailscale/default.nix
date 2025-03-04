84c3877bda498cc3cf94228a6d13373a3436b77f
topher097 <cmendres400@gmail.com>
2025-03-04 17:06:54 -0500

    Switched to using the davfs2 service rather than package

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -10,13 +10,17 @@
     ];
   };
 
+  # davfs2 for mounting taildrive
+  # LINK: https://tailscale.com/kb/1369/taildrive?tab=linux#sharing-and-accessing-folders-with-taildrive
+  services.davfs2 = {
+    enable = true;
+  };
+  users.users.${username} = {
+    extraGroups = [ "davfs2" ];
+  };
+
+
   # Setup the magic DNS (100.100.100.100) and other DNS namespaces
   networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
   networking.search = [ "tail8dc3e.ts.net" ];   # Found in the DNS section on tailscale admin console
-
-  # davfs2 for mounting taildrive
-  # LINK: https://tailscale.com/kb/1369/taildrive?tab=linux#sharing-and-accessing-folders-with-taildrive
-  environment.systemPackages = with pkgs; [
-    davfs2
-  ];
 }
