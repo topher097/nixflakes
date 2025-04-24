0d3131fd61c9a5b3c448da167358e2e8682604ff
topher097 <cmendres400@gmail.com>
2025-04-24 08:49:56 -0400

    Fix fish shell config to load new shell after direnv runs

--- modules/core/terminal/fish/default.nix
+++ modules/core/terminal/fish/default.nix
@@ -13,10 +13,12 @@
       ${pkgs.zoxide}/bin/zoxide init fish | source
       export EZA_COLORS='da=1;34:gm=1;34:Su=1;34'
       
+      # Automatically set the TERM variable to xterm-256color if using ghostty
       if test "$TERM_PROGRAM" = "ghostty"
         set -x TERM xterm-256color
       end
 
+      # Automatically set the SSH_AUTH_SOCK variable if using ssh-agent and not set
       if not set -q SSH_AUTH_SOCK
         if not set -q PRIVATE_SSH_PATH
           echo "SSH_AUTH_SOCK not set, and no PRIVATE_SSH_PATH found for SSH access."
@@ -25,6 +27,44 @@
           ssh-add "$PRIVATE_SSH_PATH"
         end
       end
+
+      # Function to load direnv changes and start a new shell
+      function direnv_load_and_start_new_shell
+        set -l direnv_output (direnv export fish)
+        if test -n "$direnv_output"
+          eval $direnv_output
+          exec fish
+        end
+      end
+
+      # Wrap the built-in cd command
+      function cd --wraps cd
+        builtin cd $argv
+        direnv_load_and_start_new_shell
+      end
+
+      # Wrap Zoxide's z command
+      function z
+        set -l dir (${pkgs.zoxide}/bin/zoxide query $argv)
+        if test $status -eq 0
+          builtin cd $dir
+          direnv_load_and_start_new_shell
+        end
+      end
+
+      # Wrap Zoxide's zi command (interactive mode)
+      function zi
+        set -l dir (${pkgs.zoxide}/bin/zoxide query -i $argv)
+        if test $status -eq 0
+          builtin cd $dir
+          direnv_load_and_start_new_shell
+        end
+      end
+
+      # https://direnv.net/docs/hook.html#fish
+      # direnv hook fish | source
+
+      eval (direnv hook fish)
 
       nitch
     '';
