5940597bbaa6b88fae06f70dfa606d98b2c5c475
topher097 <cmendres400@gmail.com>
2026-04-21 22:24:44 -0400

    fix: tailscale ssh in set flags instead of up flags

--- modules/tailscale/default.nix
+++ modules/tailscale/default.nix
@@ -13,9 +13,9 @@
     extraUpFlags = [
       # "--shields-up"
       "--operator=${username}"
-      "--ssh"
     ];
     extraSetFlags = [
+      "--ssh"
       # Route all internet traffic through Tailscale's currently suggested exit node,
       # which can be a Mullvad exit node when the add-on is available.
       "--accept-routes"        # Accept subnet routes from peers (required on Linux, default on other OSes)
